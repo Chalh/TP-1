bc19d6fd17f67357aa62ad91355c28cb4c23d594
chiheb <chiheb.elouekdi@gmail.com>
2018-10-11 13:06:02 -0400

    correction

--- PartieRegex.py
+++ PartieRegex.py
@@ -2,8 +2,26 @@
 
 import re
 import sys
-
 reload(sys)
+
+
+def get_ingredient(ligne_ingredients):
+
+    p2 = re.compile(
+        r"((^\d+\s*[.,/à]*\d*\s*([Tt]asse[s]*|[Mm][Ll]|((cuillère[s]*|c) à (soupe|café|c|s))|[Oo]z|g|[Kk]g\b|[Cc][Ll]|boîte[s]*( de conserve)*|[Ll][Bb])*)+(\sà\s\d+\s([Tt]asse[s]*|[Mm][Ll]|((cuillère[s]*|c) à (soupe|café|c|s))|[Oo]z|g\b|[Cc][Ll]|boîte*|[Ll][Bb])*)*(\s\(.*\)\s)*)(\sde*d\'*\s)*(.*)(\n)")
+    m2 = p2.search(ligne_ingredients)
+    if m2 is not None:
+        t_quantites =  m2.group(1)
+        t_ingredient = m2.group(15)
+
+    else:
+        t_quantites = ""
+        t_ingredient = ligne_ingredients[:-1]
+
+
+    return t_quantites, t_ingredient
+
+
 sys.setdefaultencoding('utf8')
 
 file = open('ingredients.txt','r')
@@ -20,19 +38,15 @@ for line in file:
     m1 = p1.search(line)
     nb_ok = nb_ok + 1
     if m1 is None:
-        p2 = re.compile(
-            r"(^\d+\s*[.,/à]*\d*\s*([Tt]asse[s]*|[Mm][Ll]|((cuillère[s]*|c) à (soupe|café|c|s))|[Oo]z|g\b|[Cc][Ll]|boîte[s]*|[Ll][Bb])*)+(\sà\s\d+\s([Tt]asse[s]*|[Mm][Ll]|((cuillère[s]*|c) à (soupe|café|c|s))|[Oo]z|g\b|[Cc][Ll]|boîte*|[Ll][Bb])*)*(\s\(.*\)\s)*(\sde*d\'*\s)*(.*\n)")
-        m2 = p2.search(line)
-        if m2 is not None:
-            ligne_ecrire = nb_ligne.__str__() +":" +line[:-1] + "  " +  ": QUANTITÉ:" + m2.group(1) + "  " + "INGRÉDIENT:" + m2.group(13) + "\n"
-            print ligne_ecrire
-            fileext.write (ligne_ecrire)
-            #fileext.write(nb_ligne.__str__() + " :INGRÉDIENT:" + m2.group(13) + "\n")
-        else:
-            ligne_ecrire =nb_ligne.__str__() +":"+ line[:-1] + "  " +  ": QUANTITÉ:" + "  " + "INGRÉDIENT:" + line[:-1] + "\n"
-            print ligne_ecrire
-            fileext.write (ligne_ecrire)
-    nb_ligne = nb_ligne + 1
+        quantite_ingredient = get_ingredient(line)
+        ligne_ecrire = line.rstrip("\n") + "  " + "; QUANTITÉ:" + quantite_ingredient[0] + " ; " + "INGRÉDIENT:" + quantite_ingredient[1]
+        nb_ligne = nb_ligne + 1
+    else:
+        ligne_ecrire = line.rstrip("\n") + "  " + "; QUANTITÉ:" +  " S/O ; INGRÉDIENT: S/O " + "\n"
+    print ligne_ecrire
+
 file.close()
 fileext.close()
+print(nb_ligne.__str__())
 print(nb_ok.__str__())
+
